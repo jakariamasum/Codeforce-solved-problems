2fe4fbafc35e3b602779109f6af1d4cfb1c47398
Jakaria Masum <100533704+jakariamasum@users.noreply.github.com>
2022-02-28 01:28:49 +0600

    Double Cola

---
+++ 82A.cpp
@@ -0,0 +1,40 @@
+#include<stdio.h>
+int   main()
+{
+int n;
+
+scanf("%d",&n);
+
+while(n>5)
+
+{
+
+n=n/2;
+
+n=n-2;
+
+}
+
+if(n==1)
+
+printf("Sheldon\n");
+
+else if(n==2)
+
+printf("Leonard\n");
+
+else if(n==3)
+
+printf("Penny\n");
+
+else if(n==4)
+
+printf("Rajesh\n");
+
+else if(n==5)
+
+printf("Howard\n");
+
+}
+
+ 
