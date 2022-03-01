4781a55e28333e9d671c4d1e3b1ca5d93851e14d
Jakaria Masum <100533704+jakariamasum@users.noreply.github.com>
2022-03-01 14:38:17 +0600

    Divisibility Problem

---
+++ 1328A.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int  t;
+    cin>>t;
+    while(t--)
+    {
+        int a,b;
+        cin>>a>>b;
+        if(a%b!=0)
+        cout<<b-(a%b)<<endl;
+        else if(a%b==0)
+        cout<<"0"<<endl;
+    }
+}
