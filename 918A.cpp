da2178a7c2b510bb0520ec5d24d9c82ad71f8bb5
Jakaria Masum <100533704+jakariamasum@users.noreply.github.com>
2022-03-01 15:00:38 +0600

    Eleven

---
+++ 918A.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace  std;
+int main()
+{
+    int i,n,l;
+   cin>>n;
+    for(i=1;i<=n;i++)
+    {
+        if(i==1 ||  i==1 || i==2||  i==3 || i==5 || i==8 || i==13 || i==21 || i==34 || i==55 || i==89 || i==144 || i==233 || i==377 || i==610 || i==987)
+        cout<<"O";
+        else
+        cout<<"o";
+    }
+    
+}
