4f0ed1927475dabe652058e57283271a10864fe2
Jakaria Masum <100533704+jakariamasum@users.noreply.github.com>
2022-03-01 14:42:12 +0600

    Pizza, Pizza, Pizza!!!

---
+++ 979A.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    
+long long int n;
+    scanf("%lld",&n);
+    long long int a=n+1;
+    if(n==0)
+    {
+        cout<<"0"<<endl;
+    }
+    else if(a%2==0)
+    {
+        cout<<a/2<<endl;
+    }
+    else
+    {
+        
+        cout<<a<<endl;
+    }}
