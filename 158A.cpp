70df4d003d9bf48e373f64283f14c71d2e54cac2
Jakaria Masum <100533704+jakariamasum@users.noreply.github.com>
2022-03-01 14:04:39 +0600

    Next Round

---
+++ 158A.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h> 
+int main() 
+    {
+     int n,k,a[50],cnt=0,i;
+     scanf("%d %d",&n,&k);
+     for(i=0; i<n; i++)
+     { scanf("%d",&a[i]); }
+     for(i=0; i<n; i++)
+     { if(a[i]>=a[k-1]&&a[i]>0)
+         cnt++;
+         }
+     printf("%d\n",cnt); 
+    return 0; 
+    }
